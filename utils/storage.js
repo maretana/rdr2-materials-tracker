1290a23d4ce8b6d991437cdf55c409602399f207
Mario Retana <maretanar@gmail.com>
2018-12-01 22:35:52 -0600

    Added functions to write and read materials count from device

---
+++ utils/storage.js
@@ -0,0 +1,30 @@
+import { AsyncStorage } from 'react-native'
+
+/**
+ * Reads the material counts object in the device
+ * @param  {String} materialKey value used to identify the material
+ * @return {Object}             The material count object
+ */
+export async function getMaterialCount (materialKey) {
+  try {
+    const value = await AsyncStorage.getItem(materialKey)
+    return value === null ? {} : JSON.parse(value)
+  } catch (error) {
+    // TODO: Catch error!!!
+    console.error(error)
+  }
+}
+
+/**
+ * Saves the material counts object in the device
+ * @param {String} materialKey value used to identify the material
+ * @param {Object} newValue    New value to save as the material counts object
+ */
+export async function setMaterialCount ({ materialKey, newValue }) {
+  try {
+    await AsyncStorage.setItem(materialKey, JSON.stringify(newValue))
+  } catch (error) {
+    // TODO: Catch error!!!
+    console.error(error)
+  }
+}
