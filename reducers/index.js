8b1b7b7cf773fe4e4ec39d7e7d6fdb3e75a93a61
Mario Retana <maretanar@gmail.com>
2018-12-10 14:38:33 -0600

    Sorting materials list by locale configuration

--- reducers/index.js
+++ reducers/index.js
@@ -2,12 +2,13 @@ import * as trapper from 'data/trapper.json'
 import * as camp from 'data/camp.json'
 import * as wae from 'data/wae.json'
 import materialsList from 'data/materials.json'
+import { sortMaterialsList } from 'utils/localization'
 import reducer from './reducers'
 
 const initialState = {
   shops: [trapper, camp, wae],
   materials: {},
-  materialsList
+  materialsList: sortMaterialsList(materialsList)
 }
 
 export default function (state = initialState, { type, payload }) {
--- utils/localization.js
+++ utils/localization.js
@@ -9,6 +9,18 @@ i18n.fallbacks = true
 i18n.translations = { en }
 i18n.locale = Localization.locale // TODO: Ckeck if user has already another locale saved.
 
+/**
+ * Gets the localized string according to the configured locale
+ * @param  {String} key    route of the string in the locale file.
+ * @param  {Object} params Params of the i18n-js library. @see {@link https://github.com/fnando/i18n-js}
+ * @return {String}        The localized string
+ */
 export function getString (key, params) {
   return i18n.t(key, params)
+}
+
+export function sortMaterialsList (materialsList) {
+  return materialsList.sort((a, b) => {
+    return getString(a.name).localeCompare(getString(b.name), i18n.locale)
+  })
 }
